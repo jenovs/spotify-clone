05873df5cff697ae0c5bcd9d04388658f9c14b41
Viktors Jenovs <jviktors@gmail.com>
2017-05-11 17:57:49 +0300

    Add playlist fetching

--- src/Playlist/Playlist.js
+++ src/Playlist/Playlist.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 // import { Redirect, Route, NavLink } from 'react-router-dom';
 
-import res from '../playlist-response.json';
+// import res from '../playlist-response.json';
 import Track from '../Track';
 
 import './Playlist.css';
 
 class Playlist extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playlistId: this.props.location.state
+    }
+  }
+
+  componentDidMount() {
+    if (!this.state.playlistId) return this.props.history.push('/');
+
+    this.fetchPlaylist.bind(this)();
+  }
+
+  fetchPlaylist() {
+    const token = JSON.parse(localStorage.getItem('token')).access_token;
+
+    fetch(`https://api.spotify.com/v1/users/spotify/playlists/${this.state.playlistId}`, {
+      headers: {Authorization: "Bearer " + token}
+    })
+    .then(res => res.json())
+    .then(json => this.setState(() => ({res: json})))
+    .catch(err => console.log('>>>>>Error', err));
+  }
+
   render() {
-    console.log('Playlist', this.props.location.state);
+    console.log('Playlist', this.state.playlistId);
+    const { res } = this.state;
+
+    if (!res) return <div>Loading...</div>
+
     return (
         <div className="Playlist__container">
           <div className="Playlist__description-container">
