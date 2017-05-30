410c2d0a74da6cd0dbbfb56097d255d55f18c296
Viktors Jenovs <jviktors@gmail.com>
2017-05-30 10:07:16 +0300

    Add PlaylistPlayButton styling

--- src/components/PlaylistPlayButton/PlaylistPlayButton.js
+++ src/components/PlaylistPlayButton/PlaylistPlayButton.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import './main.css';
 
 const PlaylistPlayButton = ({
   isPlaying,
@@ -8,11 +11,19 @@ const PlaylistPlayButton = ({
 }) => (
   <div>
     <button
+      className="playlist-play-button"
       onClick={isActivePlaylist && isPlaying ? handlePause : handlePlay}
       >
       {isActivePlaylist && isPlaying ? 'PAUSE' : 'PLAY'}
     </button>
   </div>
-)
+);
+
+PlaylistPlayButton.propTypes = {
+  isPlaying: PropTypes.bool.isRequired,
+  isActivePlaylist: PropTypes.bool.isRequired,
+  handlePlay: PropTypes.func.isRequired,
+  handlePause: PropTypes.func.isRequired,
+}
 
 export default PlaylistPlayButton;
