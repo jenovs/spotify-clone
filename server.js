58bb952ada6c269391d891afbe0401b59e8e3d38
Viktors Jenovs <jviktors@gmail.com>
2017-07-04 15:08:11 +0300

    Add server.js file

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3020, () => {
+  console.log('Server started...');
+});
