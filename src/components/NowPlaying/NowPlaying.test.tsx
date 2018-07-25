e7f33447f86ac8bf2d5f093f94c5417e14b4b1fd
Viktors Jenovs <jviktors@gmail.com>
2018-07-25 18:00:15 +0300

    Add test

--- src/components/NowPlaying/NowPlaying.test.tsx
+++ src/components/NowPlaying/NowPlaying.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { cleanup, getByAltText, render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
 
 import NowPlaying from './NowPlaying';
 import { IProps } from './NowPlaying';
@@ -10,10 +12,25 @@ const props: IProps = {
   title: 'Bar',
 };
 
+afterAll(cleanup);
+
 describe('NowPlaying component', () => {
   it('should render', () => {
     const div = document.createElement('div');
     ReactDOM.render(<NowPlaying {...props} />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should display image, artist name and song title', () => {
+    const { container, getByText } = render(<NowPlaying {...props} />);
+
+    // artist is displayed
+    expect(getByText('Foo')).toBeVisible();
+    // title is displayed
+    expect(getByText('Bar')).toBeVisible();
+    // image is displayed
+    const imageEl = getByAltText(container, 'Foo - Bar') as HTMLImageElement;
+    expect(imageEl).toBeVisible();
+    expect(imageEl.src.includes('some_url')).toBeTruthy();
   });
 });
