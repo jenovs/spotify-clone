4a780cb54a72cbd316cf0d4940959f84f4571d69
Viktors Jenovs <jviktors@gmail.com>
2018-07-25 18:50:27 +0300

    Rename file and fix fortawesome imports

--- src/components/PlayerControls/PlayerControls.tsx
+++ src/components/PlayerControls/PlayerControls.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import faPlayCircle from '@fortawesome/fontawesome-free-regular/faPlayCircle';
-import faPauseCircle from '@fortawesome/fontawesome-free-regular/faPauseCircle';
-import faStepBackward from '@fortawesome/fontawesome-free-solid/faStepBackward';
-import faStepForward from '@fortawesome/fontawesome-free-solid/faStepForward';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPauseCircle,
+  faPlayCircle,
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faStepBackward,
+  faStepForward,
+} from '@fortawesome/free-solid-svg-icons';
 
 import { Container, Controls, PlayButton, SkipButton } from './styled';
-import { Play } from '../CoverArt/PlayButton/styled';
 
 const playButton = <FontAwesomeIcon icon={faPlayCircle} />;
 const pauseButton = <FontAwesomeIcon icon={faPauseCircle} />;
 const prevButton = <FontAwesomeIcon icon={faStepBackward} />;
 const nextButton = <FontAwesomeIcon icon={faStepForward} />;
 
-const PlayerControls = props => (
+const PlayerControls = (props: any) => (
   <Container>
     <Controls>
       <SkipButton onClick={props.handlePrev} disabled={!props.hasPrevTrack}>
