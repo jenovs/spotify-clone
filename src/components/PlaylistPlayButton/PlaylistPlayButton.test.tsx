c59bd0ca51e3746157b2512befa55ef7de9d2d20
Viktors Jenovs <jviktors@gmail.com>
2018-10-06 17:00:52 +0300

    Add tests for PlaylistPlayButton

---
+++ src/components/PlaylistPlayButton/PlaylistPlayButton.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { cleanup, fireEvent, render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import PlaylistPlayButton from './PlaylistPlayButton';
+import { IProps } from './PlaylistPlayButton';
+
+const mockOnClick = jest.fn();
+
+const props: IProps = {
+  isPlaying: false,
+  onClick: mockOnClick,
+};
+
+afterEach(() => {
+  cleanup();
+  mockOnClick.mockReset();
+});
+
+describe('PlaylistPlayButton component', () => {
+  it('should render', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<PlaylistPlayButton {...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should display a button with a text PLAY', () => {
+    const { getByText } = render(<PlaylistPlayButton {...props} />);
+
+    const btn = getByText('PLAY');
+    expect(btn).toBeVisible();
+    fireEvent.click(btn);
+    expect(mockOnClick).toBeCalled();
+  });
+
+  it('should display a button with a text PAUSE', () => {
+    const { getByText } = render(
+      <PlaylistPlayButton {...props} isPlaying={true} />
+    );
+
+    const btn = getByText('PAUSE');
+    expect(btn).toBeVisible();
+    fireEvent.click(btn);
+    expect(mockOnClick).toBeCalled();
+  });
+});
