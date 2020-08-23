d7e472c8d74f2afdb75a8ccbefd3ea182d473515
Viktors Jenovs <jviktors@gmail.com>
2020-08-23 12:44:11 +0300

    test: Update TrackControlButton tests

--- src/components/TrackControlButton/TrackControlButton.test.tsx
+++ src/components/TrackControlButton/TrackControlButton.test.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { cleanup, fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import 'jest-dom/extend-expect';
 
-import TrackControlButton from './TrackControlButton';
-import { IProps } from './TrackControlButton';
+import TrackControlButton, { IProps } from './TrackControlButton';
 
 const mockHandlePause = jest.fn();
 const mockHandlePlay = jest.fn();
@@ -22,35 +20,29 @@ const props: IProps = {
 };
 
 afterEach(() => {
-  cleanup();
   mockHandlePause.mockReset();
   mockHandlePlay.mockReset();
   mockUnpause.mockReset();
 });
 
 describe('PlaylistPlayButton component', () => {
-  it('should render', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<TrackControlButton {...props} />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
-
   it('should display an ordinal number', () => {
-    const { getByText } = render(<TrackControlButton {...props} />);
+    render(<TrackControlButton {...props} />);
 
-    expect(getByText('1.')).toBeVisible();
+    expect(screen.getByText('1.')).toBeVisible();
   });
 
   // Following tests are not quite good because they test implementation (depends on the class name)
   it('should display play icon', () => {
-    const { getByRole } = render(
-      <TrackControlButton {...props} isPlaying={true} />
+    render(<TrackControlButton {...props} isPlaying={true} />);
+
+    expect(screen.getByRole('img', { hidden: true })).toHaveClass(
+      'fa-volume-up'
     );
-    expect(getByRole('img')).toHaveClass('fa-volume-up');
   });
 
   it('should display pause icon and pause on click', () => {
-    const { getByRole } = render(
+    render(
       <TrackControlButton
         {...props}
         isActive={true}
@@ -59,9 +51,12 @@ describe('PlaylistPlayButton component', () => {
       />
     );
 
-    const btn = getByRole('img');
+    const btn = screen.getByRole('img', { hidden: true });
+
     expect(btn).toHaveClass('fa-pause');
+
     fireEvent.click(btn);
+
     expect(mockHandlePause).toBeCalled();
   });
 });
